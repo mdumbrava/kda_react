2f0657e1fcc805c1b9d193b17bfc0123fddad844
mdumbrava <mihaela.dmbrv@gmail.com>
2022-04-09 15:40:19 -0400

    state

--- src/comments/comments.js
+++ src/comments/comments.js
@@ -1,20 +1,28 @@
 import "./comments.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Form from "./form";
 import UserComments from "./userComments";
 
 const Comments = () => {
-  const [title, setTitle] = useState(null);
+  const [comments, setComments] = useState("");
+  const [comment, setComment] = useState(comments[0]);
   // fetch API
   const callBackendAPI = async () => {
     let response = await fetch("/api");
     let body = await response.json();
-    setTitle(body);
-    console.log(body.comments);
+    setComments(body);
+    // console.log(body.comments);
+    console.log(comments);
   };
 
+  useEffect(() => {
+    console.log("Before Fetch");
+    callBackendAPI();
+    console.log("After Fetch");
+  }, []);
+
   // console.log(callBackendAPI());
-  callBackendAPI();
+  // callBackendAPI();
 
   return (
     <div>
@@ -22,6 +30,7 @@ const Comments = () => {
       <UserComments />
       <UserComments />
       <UserComments />
+      <div>{comment}</div>
     </div>
   );
 };
