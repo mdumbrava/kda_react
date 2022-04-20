ee87c2d96088bf077d2851f9ce534b549b6787a9
Sebastian Dumbrava <SDumbrava@evertz.com>
2022-04-20 17:22:39 -0400

    change config

--- run.py
+++ run.py
@@ -30,21 +30,22 @@ def index():
 
 @app.route('/get_commts/<page>')
 def get_commts(page):
-    page_size = 2
-    # print(page)
-    f = open('./comments.txt', 'r')
-    val_DATA = f.read()
-    f.close
+    try:
+        f = open(COMM_FILE, 'r')
+        val_DATA = f.read()
+        f.close
+    except:
+        val_DATA = '[]'
     lst_DATA = json.loads(val_DATA)
     lst_DATA.reverse()
-    val_pages = len(lst_DATA) / page_size
+    val_pages = len(lst_DATA) / PAGE_SIZE
     val_pages = math.ceil(val_pages)
     # print(val_pages)
     lst_NEW = []
-    if page_size >= len(lst_DATA) or int(page) > val_pages:
+    if PAGE_SIZE >= len(lst_DATA) or int(page) > val_pages:
         page = 1
-    val_start = page_size*(int(page)-1)
-    val_stop  = val_start + page_size
+    val_start = PAGE_SIZE*(int(page)-1)
+    val_stop  = val_start + PAGE_SIZE
     lst_NEW = lst_DATA[val_start:val_stop]
     dct_comments = {}
     dct_comments["comments"] = lst_NEW
@@ -53,9 +54,12 @@ def get_commts(page):
 
 @app.route('/save_commts', methods=['POST'])
 def save_commts():
-    f = open('./comments.txt', 'r')
-    val_DATA = f.read()
-    f.close
+    try:
+        f = open(COMM_FILE, 'r')
+        val_DATA = f.read()
+        f.close
+    except:
+        val_DATA = '[]'
     lst_DATA = json.loads(val_DATA)
     dct_DATA =  {
                 'id'     : len(lst_DATA) + 1,
@@ -63,7 +67,7 @@ def save_commts():
                 'Comment': request.form['comment'],
                 }
     lst_DATA.append(dct_DATA)
-    f = open('./comments.txt', 'w')
+    f = open(COMM_FILE, 'w')
     f.write(json.dumps(lst_DATA))
     f.close
     return redirect('/')
